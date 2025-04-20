c98bd71f5cd4c6bd5d3cf75241be45f8a217b4d0
viml kumar <vimalkg@gmail.com>
2025-04-21 05:03:04 +0530

    ztr8.vercel.app mck82.vercel.app fb.com/ztrabc x.com/etphor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,7 +65,7 @@ steps to use : 1. pls replace ԃis tekst to indiα/nepαl/sinhl/bαnglα.
           <FontPicker/>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://github.com/zawa8s/font"
+            href="https://github.com/zawa8/font"
             target="_blank"
             rel="noopener noreferrer"
           >
